bd4206ec351a68b5fe9c2aa8f54361265806fa55
Gustavo Raphael Stein <grstein.ramone@gmail.com>
2020-09-27 16:30:27 -0300

    Pagina de bem vindo e rotas. Treinamento apenas com greetings. Treinamentos proprios no dir. conversas

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 # gustavostein@outlook.com
 
 # API em Flask para interface com Chatterbot
-
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
@@ -15,27 +14,27 @@ app = Flask(__name__)
 portuguese_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter",database_uri='sqlite:///database.sqlite3')
 # Cria uma instância do treinador
 trainer = ChatterBotCorpusTrainer(portuguese_bot)
-trainer.train("chatterbot.corpus.portuguese")
 
+# greetings: https://github.com/gunthercox/chatterbot-corpus/blob/master/chatterbot_corpus/data/portuguese/greetings.yml
+# denisbot.yaml arquivo de treinamento do Denisbot desenvolvido pela equipe
+trainer.train(
+    "chatterbot.corpus.portuguese.greetings",
+    "./conversas/denisbot.yaml",
+    "./conversas/misc.yaml",
+    "./conversas/nerds.yaml"
+    )
+
+# https://chatterbot.readthedocs.io/en/stable/training.html
 # Treinar o robô com uma lista de convesas em português feito pela comunidade
 trainer = ListTrainer(portuguese_bot)
-
-# Treinar o robô com frases próprias para o sistema
-trainer.train([
-    "Olá, em que posso lhe ajudar?",
-    "Eu gostaria de iniciar um processo.",
-    "Qual é o assunto do seu processo?",
-    "Processo de Aposentadoria por tempo de contribuição.",
-    "Ah, que ótimo! Este processo precisa de alguns documentos específicos. Vamos verificar se você possui todos?",
-    "Sim",
-    "Eu preciso de uma cópia da carteira de trabalho",
-    "Aqui está",
-    "Ótimo, maravilha, fantástico!!!"
-])
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/denisbot")
+def denisbot():
+    return render_template("denisbot.html")
 
 @app.route("/get")
 def get_bot_response():
